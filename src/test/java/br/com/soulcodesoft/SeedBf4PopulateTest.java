ae4f3bad03cb160f2816ce078b891af16343e7ac
andrejobim <hqa14m2N$@$>
2023-05-13 16:38:10 -0300

    - Dependencia Log4j

--- src/test/java/br/com/soulcodesoft/SeedBf4PopulateTest.java
+++ src/test/java/br/com/soulcodesoft/SeedBf4PopulateTest.java
@@ -10,8 +10,7 @@ public class SeedBf4PopulateTest {
     public static void main(String[] args) {
         // https://battlelog.battlefield.com/bf4/servers/show/pc/SERVER_ID
         List<PlayerSeed> playerSeeds = new ArrayList<>();
-//        playerSeeds.add(new PlayerSeed("ACCOUNT_USER_EA", "PASSWORD_USER_EA"));
-        playerSeeds.add(new PlayerSeed("umjogadorfpsbf42@gmail.com", "agj14m3N$@$"));
-        new BattlefieldSeed("ea23e931-143c-4164-8334-4ae1ddfa6bbe",playerSeeds).execute();
+        playerSeeds.add(new PlayerSeed("ACCOUNT_USER_EA", "PASSWORD_USER_EA"));
+        new BattlefieldSeed("SERVER_ID",playerSeeds).execute();
     }
 }
